19393e781a7ae3156358e3514088c35d95f1ed5f
Aidrin Peraira <aidrin.peraira@gmail.com>
2025-03-08 11:06:17 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allows all HTTPS image sources
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+const {
+    default: flattenColorPalette,
+  } = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode: "class",
+    theme: {
+      extend: {},
+    },
+    plugins: [addVariablesForColors],
+  };
+  
+
+  function addVariablesForColors({ addBase, theme }: any) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+   
+    addBase({
+      ":root": newVars,
+    });
+  }
